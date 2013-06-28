894cee4dba21d0add24e20e507d429571e453e35
Catherine Powell <catherine@abakas.com>
2013-06-27 21:45:13 -0400

    Proper path name

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -68,7 +68,7 @@ end
 include_recipe 'ruby_build'
 
 ruby_build_ruby "#{node[:ruby][:full_version]}-#{node[:ruby][:patch]}" do
-  prefix_path "/usr/local/bin"
+  prefix_path "/usr/local"
 end
 
 include_recipe 'opsworks_rubygems'
