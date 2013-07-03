7b3b77fe3b5ed213a261531f314a94bf86293825
Catherine Powell <catherine@abakas.com>
2013-07-03 15:40:14 -0400

    app name

--- groupize/recipes/default.rb
+++ groupize/recipes/default.rb
@@ -1,3 +1,5 @@
 Chef::Log.info("Running asset precompile")
-rails_env = node[:deploy][application][:rails_env]
-Chef::Log.info("Precompiling for #{rails_env}")
+node[:deploy].each do |application, deploy|
+  rails_env = node[:deploy][application][:rails_env]
+  Chef::Log.info("Precompiling for #{rails_env}")
+end
