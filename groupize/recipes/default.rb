f4739b17d2294474533a0f4b5163c7f353ffd788
Catherine Powell <catherine@abakas.com>
2013-07-03 15:54:51 -0400

    create shared assets directory

--- groupize/recipes/default.rb
+++ groupize/recipes/default.rb
@@ -1,5 +1,12 @@
 Chef::Log.info("Running asset precompile")
 node[:deploy].each do |application, deploy|
   rails_env = node[:deploy][application][:rails_env]
-  Chef::Log.info("Precompiling for #{rails_env}")
+  Chef::Log.info("Ensuring shared/assets directory for #{application} app in environment #{rails_env}...")
+  directory "#{deploy[:deploy_to]}/shared/assets" do
+    group deploy[:group]
+    owner deploy[:user]
+    mode 0775
+    action :create
+    recursive true
+  end
 end
