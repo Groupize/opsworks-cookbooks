77053461e84b9de686374d084818cbdcd3ee4345
Catherine Powell <catherine@abakas.com>
2013-10-01 18:14:41 -0400

    Remove old way of doing env vars

--- groupize/recipes/default.rb
+++ groupize/recipes/default.rb
@@ -1,31 +1,3 @@
-Chef::Log.info("sourcing environment variables initializer")
-node[:deploy].each do |application, deploy|
-  deploy = node[:deploy][application]
-  execute "restart Rails app #{application}" do
-    Chef::Log.debug("restarting Rails app after setting env vars")
-    cwd deploy[:current_path]
-    command node[:opsworks][:rails_stack][:restart_command]
-    action :nothing
-  end
-
-  template "#{deploy[:deploy_to]}/current/config/initializers/env_vars.rb" do
-    source "env_vars.erb"
-    cookbook 'groupize'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables(:smtp => deploy[:smtp], :environment => deploy[:rails_env])
-    Chef::Log.debug("smtp Deploy settings: #{deploy[:smtp].inspect}")
-    Chef::Log.debug("smtp: #{@smtp.inspect}")
-
-    notifies :run, resources(:execute => "restart Rails app #{application}")
-
-    only_if do
-      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/current/config/")
-    end
-  end
-end
-
 Chef::Log.info("Set up directory for asset precompile")
 node[:deploy].each do |application, deploy|
   rails_env = node[:deploy][application][:rails_env]
