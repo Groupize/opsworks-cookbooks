391466cb39391a792708cd4940554b555c2821a9
slapshot0711 <rgrace@groupize.com>
2016-09-01 11:23:20 -0700

    Add groupize cookbook

---
+++ groupize/recipes/default.rb
@@ -0,0 +1,12 @@
+Chef::Log.info("Set up directory for asset precompile")
+node[:deploy].each do |application, deploy|
+  rails_env = node[:deploy][application][:rails_env]
+  Chef::Log.info("Ensuring shared/assets directory for #{application} app in environment #{rails_env}...")
+  directory "#{deploy[:deploy_to]}/shared/assets" do
+    group deploy[:group]
+    owner deploy[:user]
+    mode 0775
+    action :create
+    recursive true
+  end
+end
