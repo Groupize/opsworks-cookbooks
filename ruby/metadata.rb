d8cd4dc803c29eff7582b72f92a3bd5e54b26af9
slapshot0711 <rgrace@groupize.com>
2016-09-01 11:13:13 -0700

    Update ruby and rails

--- ruby/metadata.rb
+++ ruby/metadata.rb
@@ -1,10 +1,12 @@
-name        "ruby"
-description "Installs Ruby"
-maintainer  "AWS OpsWorks"
-license     "Apache 2.0"
-version     "1.0.0"
+# name        "ruby"
+# description "Installs Ruby"
+# maintainer  "AWS OpsWorks"
+# license     "Apache 2.0"
+# version     "1.0.0"
+#
+# depends 'opsworks_bundler'
+# depends 'opsworks_rubygems'
+# depends 'opsworks_initial_setup'
+# depends 'opsworks_commons'
 
-depends 'opsworks_bundler'
-depends 'opsworks_rubygems'
-depends 'opsworks_initial_setup'
-depends 'opsworks_commons'
+depends "ruby_build"
